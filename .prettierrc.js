f394853467b4d67c08f36ad635e74c9a7a8b4393
Johnson <zxmagichacker@gmail.com>
2021-03-25 23:13:32 +0800

    feat: 重写.prettierrc.js文件

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,19 @@
-/**
- * formatter配置
- */
-module.exports = {}
+// .prettierrc.js
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'none',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'auto',
+  embeddedLanguageFormatting: 'auto',
+};
