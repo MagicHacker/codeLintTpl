daaa0e43dd5cef2ef97aa5fddb0c0ed53d3b17ff
Johnson <zxmagichacker@gmail.com>
2021-04-02 00:26:45 +0800

    feat: 删除prettier arrowParens

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,9 @@
 module.exports = {
   root: true,
   extends: [
-    "alloy",
-    "alloy/react", // react项目配置
-    "prettier",
+    'alloy',
+    'alloy/react', // react项目配置
+    'prettier',
   ],
   env: {
     browser: true,
@@ -17,7 +17,7 @@ module.exports = {
     // 指定想要使用的ECMAScript版本
     ecmaVersion: 2019,
     // ECMAScript modules 模式
-    sourceType: "module",
+    sourceType: 'module',
     // 指定想使用的额外的语言特性
     ecmaFeatures: {
       // 不允许 return 语句出现在 global 环境下
@@ -28,8 +28,8 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: ["prettier"],
+  plugins: ['prettier'],
   rules: {
-    'prettier/prettier': 'error'
-  }
-}
+    'prettier/prettier': 'error',
+  },
+};
