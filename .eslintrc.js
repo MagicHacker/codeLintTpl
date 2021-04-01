979e5863110839da6f85a49e30b2eb7e1666791b
Johnson <zxmagichacker@gmail.com>
2021-04-02 00:23:24 +0800

    feat: 配置vue eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
 /**
  * eslint配置
  */
-module.exports = {}
+module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      globalReturn: false,
+      impliedStrict: true,
+    },
+  },
+  extends: [
+    'alloy',
+    'alloy/vue', // Vue项目配置
+    'prettier',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
